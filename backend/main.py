e2cbe8785bfcd7e9305cbda17f6b6a9ac933d0f6
Tauna Jassal <tanumanu173@gmail.com>
2026-07-25 21:20:54 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,8 @@ app = FastAPI(
 # Allow frontend to communicate with backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://page-pulse-tau-five.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
